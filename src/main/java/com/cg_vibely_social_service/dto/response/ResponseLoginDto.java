8945840752efd2515e77f4655ce2e7c8dd78d106
Chung Phùng Phát <chphungphat@gmail.com>
2023-08-08 16:21:28 +0700

    Feature: Authentication using email and password, filter using access token

---
+++ src/main/java/com/cg_vibely_social_service/dto/response/ResponseLoginDto.java
@@ -0,0 +1,17 @@
+package com.cg_vibely_social_service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class ResponseLoginDto {
+    private String email;
+    private String accessToken;
+}
