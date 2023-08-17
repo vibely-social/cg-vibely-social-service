83322e1b65bf81112afe4c12576aef00f2596e40
HuyVu18 <v.huy1808@gmail.com>
2023-08-17 08:06:52 +0700

    test write again file user-info-response.

--- src/main/java/com/cg_vibely_social_service/payload/response/UserInfoResponseDto.java
+++ src/main/java/com/cg_vibely_social_service/payload/response/UserInfoResponseDto.java
@@ -1,6 +1,10 @@
 package com.cg_vibely_social_service.payload.response;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,7 +16,8 @@ public class UserInfoResponseDto {
     private String email;
     private String firstName;
     private String lastName;
-    private String avatarUrl;
+    private String birthday;
     private String gender;
-    private Integer mutualFriends;
+    private String phoneNumber;
+    private String city;
 }
