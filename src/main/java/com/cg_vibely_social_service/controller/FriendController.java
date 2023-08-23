b07d26cd11b8bca979534738e027195c94fee742
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-08-23 14:55:53 +0700

    Add userId/UserImpl for extractable with SecurityContextHolder

--- src/main/java/com/cg_vibely_social_service/controller/FriendController.java
+++ src/main/java/com/cg_vibely_social_service/controller/FriendController.java
@@ -4,6 +4,7 @@ package com.cg_vibely_social_service.controller;
 import com.cg_vibely_social_service.payload.response.FriendResponseDto;
 import com.cg_vibely_social_service.service.FriendService;
 import com.cg_vibely_social_service.service.UserService;
+import com.cg_vibely_social_service.service.impl.UserImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +22,8 @@ public class FriendController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getFriendList (@PathVariable("id") Long id ) {
-        if (userService.findById(id) != null) {
+        UserImpl user = userService.getCurrentUser();
+        if (user != null && user.getId().equals(id)) {
             List<FriendResponseDto> friendList = friendService.findFriendsByUserId(id);
             return new ResponseEntity<>(friendList, HttpStatus.OK);
         } else {
